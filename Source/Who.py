e191cfd063370da2eca3112fce90d4a40bd10c2e
Soud <57543084+Soud69@users.noreply.github.com>
2021-04-28 07:57:41 +0300

    Create Who.py

---
+++ Source/Who.py
@@ -0,0 +1,115 @@
+#
+# This Is Free Tool By Soud Alanzi AKA @8Y
+# Dont Try Sell It Cuz It's Fucking Free
+# Github: https://github.com/Soud69
+# Instagram: https://instagram.com/8Y
+# Telegram: https://t.me/Soud69
+# Discord: Soud#5866
+#
+
+try:
+    import os
+    import json
+    import requests
+    import ctypes
+    import base64
+    from os import system
+    system("title " + "Soud Was Here - @8Y - Soud#5866")
+    import colorama
+    from colorama import Fore
+    colorama.init(autoreset=True)
+
+except Exception as m:
+    print("Something Went Wrong\n")
+    print(m)
+    input()
+    exit()
+logo = f"""
+{Fore.CYAN}         _______   __  {Fore.GREEN}_    _ _          ___     
+{Fore.CYAN}   ____ |  _  \ \ / / {Fore.GREEN}| |  | | |        |__ \ 
+{Fore.CYAN}  / __ \ \ V / \ V /  {Fore.GREEN}| |  | | |__   ___   ) |
+{Fore.CYAN} / / _` |/ _ \  \ /   {Fore.GREEN}| |/\| | '_ \ / _ \ / / 
+{Fore.CYAN}| | (_| | |_| | | |   {Fore.GREEN}\  /\  / | | | (_) |_|  
+{Fore.CYAN} \ \__,_\_____/ \_/    {Fore.GREEN}\/  \/|_| |_|\___/(_)  
+{Fore.CYAN}  \____/             
+"""
+print(logo)
+print(f"{Fore.GREEN}### Only GCC (Gulf Cooperation Council) ###\n")
+print(f"{Fore.RED}This Is Free Tool By Soud Alanzi And Not For Sale\n\n{Fore.RESET}{Fore.GREEN}Instagram: @8Y + @_agf\nDiscord: Soud#5866\n")
+phone_check = input("Enter Target Phone (+96569696969): ")
+if "+965" in phone_check:
+    phone = phone_check.replace("+", "")
+    url = "http://www.gooddayforpicture.com/F/BOSBNL"
+    data = {"actionn":"=3Gt@tr5_hUie_zKls=v","call1":0,"ctr":74,"hkpghyu7rgd1":"5d5b08e139aca030322b831537445cadfff6a6f2","mn2a":"IjHZnanS3JMp8l0dOz5VBpWCuL8sEQ/sfQJQB/GSFKU=","mv2":"UE9EdwbgpAxkxto=","nbrp":phone,"param_":74,"param_n3":1}
+    req = requests.post(url, data=data)
+    if 'no_search_result":"1' in req.text:
+        print(f"{Fore.RED}No Name Found For {phone_check}")
+    else:
+        name = json.loads(req.text)["names"][0]
+        namee = base64.b64decode(name).decode("utf-8")
+        print(f"{Fore.GREEN}Found Name: {namee}")
+        ctypes.windll.user32.MessageBoxW(0, f"Name: {namee}", "Message From @8Y", 1)
+elif "+966" in phone_check:
+    phone = phone_check.replace("+", "")
+    url = "http://www.worldwideplanets.com/F/BOSBNL"
+    data = {"actionn":"=3Gt@tr5_hUie_zKls=v","call1":0,"ctr":120,"hkpghyu7rgd1":"5d5b08e139aca030322b831537445cadfff6a6f2","mn2a":"ACLy5DHM7eBbZfb7czc0wafzPxl4KUFsEHjKr9Tu4n8=","mv2":"dQeN/hNgLYEIWS4=","nbrp":phone,"param_":120,"param_n3":1}
+    req = requests.post(url, data=data)
+    if 'no_search_result":"1' in req.text:
+        print(f"{Fore.RED}No Name Found For {phone_check}")
+    else:
+        name = json.loads(req.text)["names"][0]
+        namee = base64.b64decode(name).decode("utf-8")
+        print(f"{Fore.GREEN}Found Name: {namee}")
+        ctypes.windll.user32.MessageBoxW(0, f"Name: {namee}", "Message From @8Y", 1)
+elif "+974" in phone_check:
+    phone = phone_check.replace("+", "")
+    url = "http://www.wallpapersforyoutoday.com/F/BOSBNL"
+    data = {"actionn":"=3Gt@tr5_hUie_zKls=v","call1":0,"ctr":116,"hkpghyu7rgd1":"5d5b08e139aca030322b831537445cadfff6a6f2","mn2a":"b3VO6VYAAlAmg8jwwjndcgiOtgCywG4yo9FHecKfmGg=","mv2":"M6QhZF5NGa2+UqY=","nbrp":phone,"param_":116,"param_n3":1}
+    req = requests.post(url, data=data)
+    if 'no_search_result":"1' in req.text:
+        print(f"{Fore.RED}No Name Found For {phone_check}")
+    else:
+        name = json.loads(req.text)["names"][0]
+        namee = base64.b64decode(name).decode("utf-8")
+        print(f"{Fore.GREEN}Found Name: {namee}")
+        ctypes.windll.user32.MessageBoxW(0, f"Name: {namee}", "Message From @8Y", 1)
+elif "+971" in phone_check:
+    phone = phone_check.replace("+", "")
+    url = "http://www.lifewithbrave.com/F/BOSBNL"
+    data = {"actionn":"=3Gt@tr5_hUie_zKls=v","call1":0,"ctr":146,"hkpghyu7rgd1":"5d5b08e139aca030322b831537445cadfff6a6f2","mn2a":"tg9CYb2skzvhoyzV5lfhjD8qXPoLJ1TZDAROvB+4NbA=","mv2":"VMPoWAk/1dalNs8=","nbrp":phone,"param_":146,"param_n3":1}
+    req = requests.post(url, data=data)
+    if 'no_search_result":"1' in req.text:
+        print(f"{Fore.RED}No Name Found For {phone_check}")
+    else:
+        name = json.loads(req.text)["names"][0]
+        namee = base64.b64decode(name).decode("utf-8")
+        print(f"{Fore.GREEN}Found Name: {namee}")
+        ctypes.windll.user32.MessageBoxW(0, f"Name: {namee}", "Message From @8Y", 1)
+elif "+968" in phone_check:
+    phone = phone_check.replace("+", "")
+    url = "http://www.quotesandmorestuff.com/F/BOSBNL"
+    data = {"actionn":"=3Gt@tr5_hUie_zKls=v","call1":0,"ctr":120,"hkpghyu7rgd1":"5d5b08e139aca030322b831537445cadfff6a6f2","mn2a":"iu184rF/5bINZQv9//UeM4GDZdFT07/I9CRkpLUELqo=","mv2":"qzGhP0Bv9HZvrXU=","nbrp":phone,"param_":120,"param_n3":1}
+    req = requests.post(url, data=data)
+    if 'no_search_result":"1' in req.text:
+        print(f"{Fore.RED}No Name Found For {phone_check}")
+    else:
+        name = json.loads(req.text)["names"][0]
+        namee = base64.b64decode(name).decode("utf-8")
+        print(f"{Fore.GREEN}Found Name: {namee}")
+        ctypes.windll.user32.MessageBoxW(0, f"Name: {namee}", "Message From @8Y", 1)
+elif "+973" in phone_check:
+    phone = phone_check.replace("+", "")
+    url = "http://www.goimportant.com/F/BOSBNL"
+    data = {"actionn":"=3Gt@tr5_hUie_zKls=v","call1":0,"ctr":10,"hkpghyu7rgd1":"5d5b08e139aca030322b831537445cadfff6a6f2","mn2a":"wqrgjf5Z/F4VNHjePIXnOXdj/5J8E8+l2i2EXQ9tIpc=","mv2":"L0kGOXabjwXbFlU=","nbrp":phone,"param_":10,"param_n3":1}
+    req = requests.post(url, data=data)
+    if 'no_search_result":"1' in req.text:
+        print(f"{Fore.RED}No Name Found For {phone_check}")
+    else:
+        name = json.loads(req.text)["names"][0]
+        namee = base64.b64decode(name).decode("utf-8")
+        print(f"{Fore.GREEN}Found Name: {namee}")
+        ctypes.windll.user32.MessageBoxW(0, f"Name: {namee}", "Message From @8Y", 1)
+else:
+    print(f"{Fore.RED}Wrong Number Or Country Not Yet Supported")
+input("Press 'Enter' To Close..")
+exit()
